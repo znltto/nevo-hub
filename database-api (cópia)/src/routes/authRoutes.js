93b9c4332f5465135e465ca21dbbfc7b0c1abd50
Arthur Zanelatto <137451165+znltto@users.noreply.github.com>
2025-10-04 20:03:48 -0500

    Delete database-api (cópia) directory

--- database-api (cópia)/src/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const AuthController = require('../controllers/AuthController');
-
-// Rota para fazer login
-// POST /api/login
-router.post('/login', AuthController.login);
-
-module.exports = router;
--- database-api (cópia)/src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const UserController = require('../controllers/UserController');
-
-// Rota para criar um novo usuário
-// POST /api/users
-router.post('/users', UserController.create);
-
-module.exports = router;
